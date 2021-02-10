93a473b52ffe3744f46b2484d33e1215bf2c2614
UladzimirSerada <medavikiby@gmail.com>
2021-02-10 19:31:53 +0300

    (z)

--- Zadanie15.java
+++ Zadanie15.java
@@ -7,30 +7,34 @@ import java.util.Random;
 Создать массив, заполнить его случайными элементами, распечатать,
 перевернуть, и снова распечатать (при переворачивании нежелательно создавать
 еще один массив).*/
-public class Zadanie15 {
+public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
         int[] array = new int[rand.nextInt(20)];
-       // int index = 0;
-       // int number = 0;
-        for (int i = 0; i < array.length; i++) {
+
+        for (int i = 1; i < array.length; i++) {
             array[i] = rand.nextInt(20) + 1;
-        // number = array[i];
-        //index = i;
 
+            System.out.println("Случайные числа: " + Arrays.toString(array));
         }
-        System.out.println("Случайные числа: " + Arrays.toString(array));
 
-                  
+        System.out.println();
 
-            
+        int number = array.langth;
+        int temp;
 
-
+        for (int i = 0; i < number / 2; i++){
+            temp = array[number-0-1];
+            array[number-i-1] = array[i];
+            array[i] = temp;
         }
+        for(int i =0; i < array.length; i++);
+        System.out.print("перевернутые " + array[i]);
     }
+}
 
 
- 
+//}
 
 
 
